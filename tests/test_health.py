f5666494d2cb3ad9589c5d24f1b8e2d06229dfbe
Matthew Kumar <matthewkumar19@gmail.com>
2026-03-18 09:39:40 +1000

    Added some docstrings for the function calls

--- tests/test_health.py
+++ tests/test_health.py
@@ -2,14 +2,21 @@ from flask.testing import FlaskClient
 from http import HTTPStatus
 
 def test_health_status(client:FlaskClient):
-
-    url = "health/ready"
-
+    """
+    @Brief: Function tests the health status endpoint
+    @Param: client - FlaskClient object
+    @Assert: 200 - OK
+    """
+    url = "/health/ready"
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
 
 def test_health(client:FlaskClient):
-    url = "health/"
-
+    """
+    @Brief: Function tests the health endpoint
+    @Param: client - FlaskClient object
+    @Assert: 200 - OK
+    """
+    url = "/health/"
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -3,6 +3,12 @@ from http import HTTPStatus
 
 
 def test_create_task(client:FlaskClient):
+    """
+    @Brief: Function tests the create task endpoint
+    @Param: client - FlaskClient object
+    @Assert: 201 - CREATED
+    """
+    
     url = "/api/tasks"
     title = "test Create"
     description = "testing create task"
@@ -12,13 +18,23 @@ def test_create_task(client:FlaskClient):
     assert response.status_code == HTTPStatus.CREATED
 
 def test_get_task_list(client:FlaskClient):
+    """
+    @Brief: Function tests the health endpoint
+    @Param: client - FlaskClient object
+    @Assert: 200 - OK
+    """
+    
     url = "api/tasks"
-
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
 
 def test_modify_test_record(client:FlaskClient):
-    
+    """
+    @Brief: Function tests the task completed endpoint
+    @Param: client - FlaskClient object
+    @Assert: 200 - OK
+    """
+
     # Create a record to change
     createUrl = "api/tasks"
     title = "test procedure"
@@ -33,7 +49,11 @@ def test_modify_test_record(client:FlaskClient):
     assert response.status_code == HTTPStatus.OK
 
 def test_delete_record(client:FlaskClient):
-
+    """
+    @Brief: Function tests the task delete endpoint
+    @Param: client - FlaskClient object
+    @Assert: 200 - OK
+    """
     #Create something to delete
     createUrl = "api/tasks"
     title = "test procedure"
@@ -45,4 +65,3 @@ def test_delete_record(client:FlaskClient):
     deleteUrl = f"api/tasks/{taskID}"
     response = client.delete(deleteUrl)
     assert response.status_code == HTTPStatus.OK
-    
