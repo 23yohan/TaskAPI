97b04a801497723059d298de0a0cf0b05b2f3099
Matthew Kumar <matthewkumar19@gmail.com>
2026-03-17 16:38:16 +1000

    Added helper function to convrt obj into dict. Also removed some unused imports

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import Integer, String, ForeignKey, Boolean, PrimaryKeyConstraint, Column, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean, DateTime
 from datetime import datetime as dt
 from datetime import timezone as tz
 from app import db
@@ -17,4 +17,19 @@ class Tasks(db.Model):
     updated_at: Mapped[dt]    = mapped_column(DateTime(timezone=True), 
                                             onupdate=lambda: dt.now(tz.utc), 
                                             nullable=True)
+
+    def to_dict(self):
+        """
+        @Brief: Function returns the values from the db in a dict format
+        @Param: self - Instance of the class
+        @Return: res - A dictionary of the request
+        """
+        return {
+            "id" : self.id,
+            "title" : self.title,
+            "description" : self.description,
+            "completed" : self.completed,
+            "created_at" : self.created_at.isoformat(),
+            "updated_at" : self.updated_at.isoformat() if self.updated_at else None
+        }
 
