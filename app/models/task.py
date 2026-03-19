595aefec71b7ceda10122899d84e8c3e37d14ff7
Matthew Kumar <matthewkumar19@gmail.com>
2026-03-19 10:09:36 +1000

    Added user parameters to tasks table and defined relationship betweeen users and tasks

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, String, Boolean, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, Relationship
+from sqlalchemy import Integer, String, Boolean, DateTime, ForeignKey
 from datetime import datetime as dt
 from datetime import timezone as tz
 from app import db
@@ -17,6 +17,11 @@ class Tasks(db.Model):
     updated_at: Mapped[dt]    = mapped_column(DateTime(timezone=True), 
                                             onupdate=lambda: dt.now(tz.utc), 
                                             nullable=True)
+    created_by : Mapped[int]  = mapped_column("created_by", Integer, ForeignKey("users.user_id"), nullable=False)
+    assigned_to : Mapped[int]  = mapped_column("assigned_to", Integer, ForeignKey("users.user_id"), nullable=True)
+
+    creator = Relationship("Users", foreign_keys=[created_by], backref=["created_tasks"])
+    assignee = Relationship("Users", foreign_keys=[assigned_to], backref="assigned_tasks")
 
     def to_dict(self):
         """
@@ -30,6 +35,8 @@ class Tasks(db.Model):
             "description" : self.description,
             "completed" : self.completed,
             "created_at" : self.created_at.isoformat(),
-            "updated_at" : self.updated_at.isoformat() if self.updated_at else None
+            "updated_at" : self.updated_at.isoformat() if self.updated_at else None,
+            "created_by" : self.created_by,
+            "assigned_to": self.assigned_to if self.assigned_to else None
         }
 
