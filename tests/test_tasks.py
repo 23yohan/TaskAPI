f42abaf79b0c69ea226de601c3f04524e266b4c1
Matthew Kumar <matthewkumar19@gmail.com>
2026-03-26 12:31:26 +1000

    Updated tests with using the pyFixture for creating users. Also updated the route to omitt the api

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -2,17 +2,17 @@ from flask.testing import FlaskClient
 from http import HTTPStatus
 
 
-def test_create_task(client:FlaskClient):
+def test_create_task(client:FlaskClient, test_user: str):
     """
     @Brief: Function tests the create task endpoint
     @Param: client - FlaskClient object
     @Assert: 201 - CREATED
     """
-    
-    url = "/api/tasks"
+
+    url = "/tasks/create"
     title = "test Create"
     description = "testing create task"
-    data = {"title" : title, "description": description}
+    data = {"title" : title, "description": description, "user" : test_user}
 
     response = client.post(url, json=data)
     assert response.status_code == HTTPStatus.CREATED
@@ -24,11 +24,11 @@ def test_get_task_list(client:FlaskClient):
     @Assert: 200 - OK
     """
     
-    url = "api/tasks"
+    url = "/tasks/"
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
 
-def test_modify_test_record(client:FlaskClient):
+def test_modify_test_record(client:FlaskClient, test_user: str):
     """
     @Brief: Function tests the task completed endpoint
     @Param: client - FlaskClient object
@@ -36,32 +36,33 @@ def test_modify_test_record(client:FlaskClient):
     """
 
     # Create a record to change
-    createUrl = "api/tasks"
-    title = "test procedure"
-    description = "test the modify function"
-    data = {"title" : title, "description" : description}
+    createUrl = "/tasks/create"
+    title = "test Create"
+    description = "testing create task"
+    data = {"title" : title, "description": description, "user" : test_user}
     clientResponse = client.post(createUrl, json=data)
     taskID = clientResponse.get_json().get("id")
 
     # Modify the test record - completion status
-    url = f"api/tasks/{taskID}"
+    url = f"/tasks/{taskID}"
     response = client.patch(url)
     assert response.status_code == HTTPStatus.OK
 
-def test_delete_record(client:FlaskClient):
+def test_delete_record(client:FlaskClient, test_user: str):
     """
     @Brief: Function tests the task delete endpoint
     @Param: client - FlaskClient object
     @Assert: 200 - OK
     """
+
     #Create something to delete
-    createUrl = "api/tasks"
-    title = "test procedure"
-    description = "test the delete function"
-    data = {"title" : title, "description" : description}
+    createUrl = "/tasks/create"
+    title = "test Create"
+    description = "testing create task"
+    data = {"title" : title, "description": description, "user" : test_user}
     clientResponse = client.post(createUrl, json=data)
     taskID = clientResponse.get_json().get("id")
 
-    deleteUrl = f"api/tasks/{taskID}"
+    deleteUrl = f"/tasks/{taskID}"
     response = client.delete(deleteUrl)
     assert response.status_code == HTTPStatus.OK
