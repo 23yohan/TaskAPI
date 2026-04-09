927f5a5ae951151058a424ff15b9570de7cd532b
Matthew Kumar <matthewkumar19@gmail.com>
2026-04-09 13:59:42 +1000

    Added a temp user to be deleted for the delete test. Also parsed the jwt authenitcation on the API call

--- tests/test_users.py
+++ tests/test_users.py
@@ -14,11 +14,23 @@ def test_create_user(client: FlaskClient):
     response = client.post(url, json=data)
     assert response.status_code == HTTPStatus.CREATED
 
-def test_delete_user(client: FlaskClient, test_user: str):
+def test_delete_user(client: FlaskClient, auth_headers:dict):
     
+    # Create a user to delete
+    url = "/users/create"
+    data = {
+        "first_name": "test",
+        "last_name": "user",
+        "email": "test_user@taskapi.com",
+        "password": "T3$tusr123"
+    }
+    response = client.post(url, json=data)
+    testUser = response.get_json().get("email")
+
+
     # Now delete user
-    deleteUrl = f"/users/delete_user/{test_user}"
-    delResponse = client.delete(deleteUrl)
+    deleteUrl = f"/users/delete_user/{testUser}"
+    delResponse = client.delete(deleteUrl, headers=auth_headers)
     assert delResponse.status_code == HTTPStatus.OK
 
 
