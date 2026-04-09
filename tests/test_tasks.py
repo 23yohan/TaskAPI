dd2b4cd5f1b35f785feb33911ac31273b78be761
Matthew Kumar <matthewkumar19@gmail.com>
2026-04-09 13:45:31 +1000

    Added headers for the test API calls

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -2,7 +2,7 @@ from flask.testing import FlaskClient
 from http import HTTPStatus
 
 
-def test_create_task(client:FlaskClient, test_user: str):
+def test_create_task(client:FlaskClient, auth_headers: dict):
     """
     @Brief: Function tests the create task endpoint
     @Param: client - FlaskClient object
@@ -12,12 +12,12 @@ def test_create_task(client:FlaskClient, test_user: str):
     url = "/tasks/create"
     title = "test Create"
     description = "testing create task"
-    data = {"title" : title, "description": description, "user" : test_user}
+    data = {"title" : title, "description": description}
 
-    response = client.post(url, json=data)
+    response = client.post(url, json=data, headers=auth_headers)
     assert response.status_code == HTTPStatus.CREATED
 
-def test_get_task_list(client:FlaskClient):
+def test_get_task_list(client:FlaskClient, auth_headers:dict):
     """
     @Brief: Function tests the health endpoint
     @Param: client - FlaskClient object
@@ -25,10 +25,10 @@ def test_get_task_list(client:FlaskClient):
     """
     
     url = "/tasks/"
-    response = client.get(url)
+    response = client.get(url, headers=auth_headers)
     assert response.status_code == HTTPStatus.OK
 
-def test_modify_test_record(client:FlaskClient, test_task: dict):
+def test_modify_test_record(client:FlaskClient, test_task: dict, auth_headers:dict):
     """
     @Brief: Function tests the task completed endpoint
     @Param: client - FlaskClient object
@@ -39,18 +39,18 @@ def test_modify_test_record(client:FlaskClient, test_task: dict):
 
     # Modify the test record - completion status
     url = f"/tasks/{taskID}"
-    response = client.patch(url)
+    response = client.patch(url, headers=auth_headers)
     assert response.status_code == HTTPStatus.OK
 
-def test_delete_record(client:FlaskClient, test_task: dict):
+def test_delete_record(client:FlaskClient, test_task: dict, auth_headers: dict):
     """
     @Brief: Function tests the task delete endpoint
     @Param: client - FlaskClient object
     @Assert: 200 - OK
     """
-
+    print(test_task)
     taskID = test_task.get("id")
 
-    deleteUrl = f"/tasks/{taskID}"
-    response = client.delete(deleteUrl)
+    deleteUrl = f"/tasks/delete/{taskID}"
+    response = client.delete(deleteUrl, headers=auth_headers)
     assert response.status_code == HTTPStatus.OK
